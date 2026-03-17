6be0f44912c368aa666da0dbb64ba727422b9285
FroGGlik <denis.klaus2007@yandex.ru>
2026-03-17 19:25:02 +0300

    register Product, Category

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register
+class AdminProducts(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'category', 'updated', 'created']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
